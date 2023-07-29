d0974ee940365c26e5a6f16bfa82d41886a159fb
Amir Hossein Kaveh <amirk1998@outlook.com>
2023-07-29 11:09:41 +0330

    Image Slider has been created

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -2,12 +2,39 @@
 
 import ImageSlider from '@/components/ImageSlider';
 
+const slides = [
+  {
+    src: '/images/ImageSlider/Image(1).jpg',
+    alt: 'Image (1)',
+  },
+  {
+    src: '/images/ImageSlider/Image(2).jpg',
+    alt: 'Image (2)',
+  },
+  {
+    src: '/images/ImageSlider/Image(3).jpg',
+    alt: 'Image (3)',
+  },
+  {
+    src: '/images/ImageSlider/Image(4).jpg',
+    alt: 'Image (4)',
+  },
+  {
+    src: '/images/ImageSlider/Image(5).jpg',
+    alt: 'Image (5)',
+  },
+  {
+    src: '/images/ImageSlider/Image(6).jpg',
+    alt: 'Image (6)',
+  },
+];
+
 export default function Home() {
   return (
     <main>
       {/* <h1 className='text-4xl font-bold'>Home Page</h1> */}
       <div>
-        <ImageSlider />
+        <ImageSlider slides={slides} />
       </div>
     </main>
   );
