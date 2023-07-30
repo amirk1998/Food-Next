e34bd794e81dd01469e041dde298066c3306e56e
Amir Hossein Kaveh <amirk1998@outlook.com>
2023-07-30 12:56:22 +0330

    Create Intro Section in Home Page (Part 01)

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -3,6 +3,8 @@
 import SearchField from '@/common/SearchField';
 import FoodComponent from '@/components/FoodComponent';
 import ImageSlider from '@/components/ImageSlider';
+import UserOutline from '@/public/icons/UserOutline';
+import Image from 'next/image';
 
 const slides = [
   {
@@ -79,12 +81,51 @@ export default function Home() {
       <h1 className='mb-3 mt-6 text-center text-base font-bold leading-snug'>
         منوی رستوران
       </h1>
-      <div className='grid w-full grid-cols-2 grid-rows-2 gap-4 px-5 lg:px-12 xl:grid-cols-4 xl:grid-rows-1'>
+      <div className='container grid w-full grid-cols-2 grid-rows-2 gap-4 px-5 lg:px-12 xl:grid-cols-4 xl:grid-rows-1'>
         {foodItems.map((item, index) => {
           return <FoodComponent item={item} key={item.id} />;
         })}
       </div>
-      <div className='mt-6 h-[340px] w-full bg-red-100'>Hello</div>
+      <div className='relative mt-6 flex h-[340px] w-full lg:h-[390px]'>
+        <div className='absolute inset-0 z-0 max-h-[340px] w-full lg:max-h-[390px]'>
+          <Image
+            alt='Mountains'
+            src='/images/IntroImg.png'
+            className='blur-[2px] brightness-50'
+            quality={70}
+            fill
+            style={{
+              objectFit: 'cover',
+            }}
+          />
+        </div>
+        <div className='z-10 flex flex-col px-5 py-4 text-white lg:flex-row lg:justify-between'>
+          <h1 className='mb-2 text-base font-bold leading-7 md:mb-4 md:text-lg lg:mb-6 lg:text-2xl'>
+            رستوران‌های زنجیره‌ای ترخینه
+          </h1>
+          <div className='mb-2 w-80 text-justify text-[10px] font-normal capitalize leading-[18px] text-white md:w-96 md:text-base lg:w-[600px] lg:max-w-[600px] lg:text-xl'>
+            مهمان‌نوازی یکی از مهم‌ترین مشخصه‌های ایرانیان است و باعث افتخار
+            ماست که بیش از 20 سال است خدمت‌گزار مردم شریف ایران هستیم. ما در
+            رستوران‌های زنجیره‌ای ترخینه همواره تلاش کردیم که در محیطی اصیل بر
+            پایه معماری و طراحی مدرن در کنار طبیعتی دلنواز، غذایی سالم و درخور
+            شان شما عزیزان ارائه دهیم.
+          </div>
+          <div className='mb-6 flex items-center justify-between'>
+            <div className='flex-1'></div>
+            <button className='inline-flex h-8 w-[152px] items-center justify-center rounded border border-white p-2 text-white'>
+              اطلاعات بیشتر
+            </button>
+          </div>
+          <div className='grid grid-cols-2 grid-rows-2'>
+            <div className='flex w-[117px] flex-col items-center gap-y-1 lg:w-[184px]'>
+              <UserOutline />
+              <span className='text-xs font-normal leading-snug text-white'>
+                پرسنلی مجرب و حرفه‌ای
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
