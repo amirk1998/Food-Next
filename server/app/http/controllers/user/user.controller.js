cb6786d89b382fcd05652d38b1bab428ad1312d7
Amir Hossein Kaveh <amirk1998@outlook.com>
2023-08-11 12:09:18 +0330

    Welcome message has been updated

--- server/app/http/controllers/user/user.controller.js
+++ server/app/http/controllers/user/user.controller.js
@@ -87,7 +87,7 @@ class userAuthController extends Controller {
     // await setAuthCookie(res, user); // set httpOnly cookie
     await setAccessToken(res, user);
     await setRefreshToken(res, user);
-    let WELLCOME_MESSAGE = `کد تایید شد، به شاپ نکست خوش آمدید`;
+    let WELLCOME_MESSAGE = `کد تایید شد، به فود نکست خوش آمدید`;
     if (!user.isActive)
       WELLCOME_MESSAGE = `کد تایید شد، لطفا اطلاعات خود را تکمیل کنید`;
 
